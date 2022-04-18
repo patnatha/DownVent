b061a3585e07fe6d61b6d9c9a52df7309294578a
nathan patel <nthnpatel@gmail.com>
2022-04-18 14:24:25 -0400

    to run autoonomously

--- aestiva.py
+++ aestiva.py
@@ -5,10 +5,18 @@ import datetime
 import redcap as rc
 
 ser = None
-def open_serial():
-    global ser
-    ser = serial.Serial(port=the_port, baudrate=19200, bytesize=serial.SEVENBITS,
-                        parity=serial.PARITY_ODD, stopbits=serial.STOPBITS_ONE)
+last_ser_read = None
+def open_serial(the_port):
+    try:
+        global ser
+        ser = serial.Serial(port=the_port, baudrate=19200, 
+                            write_timeout=5, timeout=2,
+                            bytesize=serial.SEVENBITS,
+                            parity=serial.PARITY_ODD, 
+                            stopbits=serial.STOPBITS_ONE)
+    except Exception as err:
+        ser = None
+        print("OPEN PORT ERROR,", err)
 
 def close_serial():
     global ser
@@ -29,21 +37,40 @@ def search_coms():
 
 def read_resp():
     global ser
+    global last_ser_read
     theMsg = ''
     while(True):
         try:
             theByte = ser.read(1)
+            if(theByte == b''): raise Exception("read_timeout")
+            
             decodeByte = theByte.decode('utf-8')
             if(decodeByte == '\r'):
+                #Bank the last time was read from serial
+                last_ser_read = datetime.datetime.now()
                 break
             else:
                 theMsg += decodeByte
         except Exception as err:
-            print("read_resp, ERROR:", err)
+            #If error is not a read timeout then print the error
+            if("read_timeout" not in str(err)): 
+                print("read_resp, ERROR:", err)
+                close_serial()
+
+            #If it has been greater than 30 seconds since last serial read
+            if(last_ser_read != None and 
+               ((datetime.datetime.now() - last_ser_read).total_seconds() > 20)):
+                close_serial()
+
+            #Return blank message
+            theMsg = None
+            break
 
     return(theMsg)
 
 def send_msg(the_bytes):
+    if(ser == None): return(None)
+
     ser.write(b'\x1B')
     sendy = ser.write(bytes(the_bytes, "utf-8"))
     ser.write(bytes('\r', "utf-8"))
@@ -59,7 +86,7 @@ def convert_int(the_string, sind, eind):
         theStr = the_string[sind:eind]
         return(int(theStr))
     except Exception as err:
-        print("convert_int", sind, eind)
+        #print("convert_int", sind, eind, err)
         return(None)
 
 sendDict = {}
@@ -68,28 +95,40 @@ def default_send_dict():
     sendDict = {"datetime": None, "measured": None, "set": None}
 
 def cur_time_string():
-    return(datetime.datetime.now().strftime("%Y-%m-%d %H:%M%:S"))
+    return(datetime.datetime.now().strftime("%Y-%m-%d %H:%M:%S"))
 
+no_port_sleep = 30
 while(True):
     if(ser == None):
         found_port = search_coms()
         if(found_port != None):
-            print("FOUND COM:", found_port)
-            open_serial()
+            print("FOUND PORT:", found_port)
+            open_serial(found_port)
 
             resp = send_msg("VTDw")
-            print("DISABLE CHECKSUM:", resp)
-
-            resp = send_msg("VTX ")
-            print("SET AUTO MODE:", resp)
+            if(resp != None):
+                print("DISABLE CHECKSUM:", resp)
+            
+                resp = send_msg("VTX ")
+                if(resp != None):
+                    print("SET AUTO MODE:", resp)
+                else:
+                    close_serial()
+                    time.sleep(no_port_sleep)
+            else:
+                close_serial()
+                time.sleep(no_port_sleep)
 
             default_send_dict()
         else:
             print("NO AVAILABLE PORTS")
-            time.sleep(60)
+            time.sleep(no_port_sleep)
     else:
         theRes = read_resp()
-        print(cur_time_string, "-", theRes)
+        if(theRes == None):
+            continue
+
+        print(cur_time_string(), "-", theRes)
         
         if(theRes.startswith(":VTD")):
             tv = convert_int(theRes, 4, 8)
@@ -98,10 +137,10 @@ while(True):
             mv = convert_int(theRes, 8, 12)
             print("Measured Minute Volume:", mv)
 
-            rr = convert_int(theRes,theRes, 12, 15)
+            rr = convert_int(theRes, 12, 15)
             print("Measured RR:", rr)
 
-            o2 = convert_int(theRes, theRes, 15, 18)
+            o2 = convert_int(theRes, 15, 18)
             print("Mesured O2:", o2)
 
             maxPres = convert_int(theRes, 18, 21)
@@ -124,7 +163,8 @@ while(True):
                 indByte = False
             else:
                 indByte = False
-                
+            print("Measured Data Status:", indByte)    
+
             if(sendDict["datetime"] == None):
                 sendDict["datetime"] = cur_time_string()
             sendDict["measured"] = {}
@@ -136,6 +176,7 @@ while(True):
             sendDict["measured"]["meas_insp_plat"] = inspPlat
             sendDict["measured"]["meas_mean_pres"] = meanPres
             sendDict["measured"]["meas_min_pres"] = minPres
+            sendDict["measured_ind_byte"] = indByte
         elif(theRes.startswith(":VTQ")):
             tv = convert_int(theRes, 4, 8)
             print("Set Tidal Volume:", tv)
@@ -144,8 +185,8 @@ while(True):
             print("Set RR:", rr)
 
             try:
-                itoe = theRes[11, 15]
-                strVal = itoe[0:3] + "." + itoe[3]
+                itoe = theRes[11:15]
+                strVal = itoe[0:3] + "." + itoe[3:4]
                 itoe = float(strVal)
             except Exception as err:
                 itoe = None
@@ -173,6 +214,10 @@ while(True):
                 ventMode = None
             print("Set Vent Mode:", ventMode)
 
+            #print(len(theRes))
+            #print(theRes[45:58])
+            #print(theRes[45:58].encode("utf-8"))
+
             if(sendDict["datetime"] == None):
                 sendDict["datetime"] = cur_time_string()
             sendDict["set"] = {}
@@ -183,8 +228,18 @@ while(True):
             sendDict["set"]["set_insp_pres"] = inspPres
             sendDict["set"]["set_vent_mode"] = ventMode
 
-        if(sendDict["measured"] != None and sendDict["set"] != None):
-            #Send redcap
-            print("Send Record")        
+        if(sendDict["datetime"] != None and
+                sendDict["measured"] != None and 
+                sendDict["set"] != None):
+
+            if(sendDict["measured_ind_byte"]):
+                toSend = {}
+                for x in sendDict["set"]:
+                    toSend[x] = sendDict["set"][x]
+                for y in sendDict["measured"]:
+                    toSend[y] = sendDict["measured"][y]
+                rcres = rc.post_redcap(toSend)
+                print("Send Record:", rcres)
+
             default_send_dict()
 
